5a30bd8ea207db8cdab048f3c0a8b629f1d844ea
Ethan-Hang <2125846436@qq.com>
2025-04-08 23:13:16 +0800

    温湿度显示

--- exp3/User/main.c
+++ exp3/User/main.c
@@ -2,34 +2,56 @@
 #include "Delay.h"
 #include "OLED.h"
 
+void OLED_ShowBasicInfo(void);
+void OLED_ShowTemHum(void);
+
 int main()
 {
     OLED_Init();
     while (1)
     {
-        // OLED_ShowChar(1, 1, 'a');
-        // OLED_ShowString(1, 3, "Hello World!");
-        // OLED_ShowNum(2, 1, 12345, 4);
-        // OLED_ShowSignedNum(2, 7, -66, 2);
-        // OLED_ShowHexNum(3, 1, 0xAA55, 4);
-        // OLED_ShowBinNum(4, 1, 0xAA55, 16);
-
-        for (uint8_t i = 0; i <= 2; i++)
-        {
-            OLED_ShowCN(1, i + 1, i);
-        }
-        OLED_ShowNum(1, 7, 233, 3);
-
-        for (uint8_t i = 3; i <= 5; i++)
-        {
-            OLED_ShowCN(2, i - 2, i);
-        }
-        OLED_ShowNum(2, 7, 3230515068, 10);
-        for (uint8_t i = 6; i <= 7; i++)
-        {
-            OLED_ShowCN(3, i - 5, i);
-        }
-        OLED_ShowNum(3, 5, 3230621101, 10);
+        //显示班级名字学号
+        // OLED_ShowBasicInfo();
+        OLED_ShowTemHum();
 
     }
+}
+
+void OLED_ShowBasicInfo(void)
+{
+    for (uint8_t i = 0; i <= 2; i++)
+    {
+        OLED_ShowCN(1, i + 1, i);
+    }
+    OLED_ShowNum(1, 7, 233, 3);
+    
+    for (uint8_t i = 3; i <= 5; i++)
+    {
+        OLED_ShowCN(2, i - 2, i);
+    }
+    OLED_ShowNum(2, 7, 3230515068, 10);
+    for (uint8_t i = 6; i <= 7; i++)
+    {
+        OLED_ShowCN(3, i - 5, i);
+    }
+    OLED_ShowNum(3, 5, 3230621101, 10);
+}
+
+void OLED_ShowTemHum(void)
+{
+    for (uint8_t i = 8; i <= 11; i++)
+    {
+        OLED_ShowCN(1, i - 7, i);
+    }
+    OLED_ShowChar(1, 9, ':'); 
+    OLED_ShowNum(1, 10, 10, 3);
+    OLED_ShowCN(1, 7, 12);
+
+    for (uint8_t i = 13; i <= 16; i++)
+    {
+        OLED_ShowCN(2, i - 12, i);
+    }
+    OLED_ShowChar(2, 9, ':'); 
+    OLED_ShowNum(2, 10, 10, 3);
+    OLED_ShowChar(2, 13, '%');
 }
