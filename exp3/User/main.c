2e4f0d2cdabb87486caed7a4a723e60d37bafe4c
Ethan-Hang <2125846436@qq.com>
2025-04-08 15:05:25 +0800

    汉字显示

--- exp3/User/main.c
+++ exp3/User/main.c
@@ -7,11 +7,24 @@ int main()
     OLED_Init();
     while (1)
     {
-        OLED_ShowChar(1, 1, 'a');
-        OLED_ShowString(1, 3, "Hello World!");
-        OLED_ShowNum(2, 1, 12345, 4);
-        OLED_ShowSignedNum(2, 7, -66, 2);
-        OLED_ShowHexNum(3, 1, 0xAA55, 4);
-        OLED_ShowBinNum(4, 1, 0xAA55, 16);
-    } 
+        // OLED_ShowChar(1, 1, 'a');
+        // OLED_ShowString(1, 3, "Hello World!");
+        // OLED_ShowNum(2, 1, 12345, 4);
+        // OLED_ShowSignedNum(2, 7, -66, 2);
+        // OLED_ShowHexNum(3, 1, 0xAA55, 4);
+        // OLED_ShowBinNum(4, 1, 0xAA55, 16);
+
+        for (uint8_t i = 0; i <= 2; i++)
+        {
+            OLED_ShowCN(1, i + 1, i);
+        }
+        OLED_ShowNum(1, 7, 233, 3);
+
+        for (uint8_t i = 3; i <= 5; i++)
+        {
+            OLED_ShowCN(2, i - 2, i);
+        }
+        OLED_ShowNum(2, 5, 3230515068, 10);
+
+    }
 }
