6febb7e3f3b796d84171c720481725f5562c792e
Ethan-Hang <2125846436@qq.com>
2025-04-15 10:07:10 +0800

    exp5

--- exp5/User/main.c
+++ exp5/User/main.c
@@ -1,17 +1,33 @@
 #include "stm32f10x.h" // Device header
 #include "Delay.h"
 #include "OLED.h"
+#include "Serial.h"
+#include "AD.h"
+
+uint16_t ADValue;			//定义AD值变量
+float Voltage;				//定义电压变量
 
 int main()
 {
-    OLED_Init();
-    while (1)
-    {
-        OLED_ShowChar(1, 1, 'a');
-        OLED_ShowString(1, 3, "Hello World!");
-        OLED_ShowNum(2, 1, 12345, 4);
-        OLED_ShowSignedNum(2, 7, -66, 2);
-        OLED_ShowHexNum(3, 1, 0xAA55, 4);
-        OLED_ShowBinNum(4, 1, 0xAA55, 16);
-    } 
+	/*模块初始化*/
+	OLED_Init();			//OLED初始化
+	AD_Init();				//AD初始化
+    Serial_Init();
+	
+	/*显示静态字符串*/
+	OLED_ShowString(1, 1, "ADValue:");
+	OLED_ShowString(2, 1, "Voltage:0.00V");
+	
+	while (1)
+	{
+		ADValue = AD_GetValue();					//获取AD转换的值
+		Voltage = (float)ADValue / 4095 * 3.3;		//将AD值线性变换到0~3.3的范围，表示电压
+		
+		OLED_ShowNum(1, 9, ADValue, 4);				//显示AD值
+		OLED_ShowNum(2, 9, Voltage, 1);				//显示电压值的整数部分
+		OLED_ShowNum(2, 11, (uint16_t)(Voltage * 100) % 100, 2);	//显示电压值的小数部分
+        Serial_Printf("ADValue:%f\r\n", Voltage);	//通过串口打印AD值
+		
+		Delay_ms(100);			//延时100ms，手动增加一些转换的间隔时间
+	}
 }
